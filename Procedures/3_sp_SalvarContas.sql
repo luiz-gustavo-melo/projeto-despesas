f75c0fcb9dfab8ef5f7f932643582c41cafa5452
Gustavo <lgmelo1998@gmail.com>
2024-07-27 05:52:32 -0300

    criação procedure de salvar contas

---
+++ Procedures/3_sp_SalvarContas.sql
@@ -0,0 +1,48 @@
+USE Despesas 
+GO
+
+CREATE PROCEDURE sp_SalvarContas @ID_CONTAS INT, @FK_USUARIOS INT, @NOME VARCHAR(30), @ICONE INT
+AS
+
+BEGIN TRY
+	IF (@ID_CONTAS = 0)
+	BEGIN 
+		INSERT Contas(FK_USUARIOS, NOME, ICONE)
+		SELECT @FK_USUARIOS, @NOME, @ICONE
+
+		SET @ID_CONTAS = SCOPE_IDENTITY()
+	END 
+ELSE 
+	BEGIN
+		UPDATE Contas
+		SET NOME = @NOME, ICONE = @ICONE
+		WHERE (ID_CONTAS = @ID_CONTAS)
+	END
+
+END TRY
+
+-- tratando erros;
+BEGIN CATCH
+	DECLARE @ERRO_COMPLETO VARCHAR(2000)
+
+	SET @ERRO_COMPLETO = CONCAT(
+		'Número do Erro: ',			ERROR_NUMBER(),			CHAR(13),
+		'Descrição do Erro: ',		ERROR_MESSAGE(),		CHAR(13),
+		'Severidade do Erro: ',		ERROR_SEVERITY(),		CHAR(13),
+		'Nome do procedimento: ',	ERROR_PROCEDURE(),		CHAR(13),
+		'Linha do Error: ',			ERROR_LINE(),			CHAR(13))
+
+	DECLARE @ESTADO INT = ERROR_STATE();
+
+	THROW 50000, @ERRO_COMPLETO, @ESTADO
+END CATCH
+
+
+/*
+EXEC sp_SalvarContas @ID_CONTAS = 0, @FK_USUARIOS = 1 , @NOME = 'Inter', @ICONE = 1
+
+SELECT * FROM Usuarios
+SELECT * FROM Contas
+
+sp_help 'Contas'
+*/
